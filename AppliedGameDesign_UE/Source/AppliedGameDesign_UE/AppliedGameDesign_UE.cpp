22b7804aac38f1b6bf89d0d449efdaf9afeca064
oliveeah <olliebennettx@gmail.com>
2025-10-02 14:17:37 +0000

    added project files

---
+++ AppliedGameDesign_UE/Source/AppliedGameDesign_UE/AppliedGameDesign_UE.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AppliedGameDesign_UE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AppliedGameDesign_UE, "AppliedGameDesign_UE" );
+
+DEFINE_LOG_CATEGORY(LogAppliedGameDesign_UE)
+ 
---
+++ AppliedGameDesign_UE/Source/AppliedGameDesign_UE/AppliedGameDesign_UEGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AppliedGameDesign_UEGameMode.h"
+
+AAppliedGameDesign_UEGameMode::AAppliedGameDesign_UEGameMode()
+{
+	// stub
+}
