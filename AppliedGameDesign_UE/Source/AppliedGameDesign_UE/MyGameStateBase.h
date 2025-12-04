bf92c33cf2c237b07b02978e3257ef836c9e6280
oliveeah <olliebennettx@gmail.com>
2025-12-04 14:17:32 +0000

    fixed win issue

---
+++ AppliedGameDesign_UE/Source/AppliedGameDesign_UE/MyGameStateBase.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "MyGameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APPLIEDGAMEDESIGN_UE_API AMyGameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+
+//public:
+//
+//    AMyGameStateBase();
+//
+//    UPROPERTY(Replicated, BlueprintReadOnly)
+//    bool bOddIsFull;
+//
+//    UPROPERTY(Replicated, BlueprintReadOnly)
+//    bool bEvenIsFull;
+//
+//    virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+};
